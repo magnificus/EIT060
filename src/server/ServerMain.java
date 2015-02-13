8b4123d22ad227097aceabeebaaf7a7ceac801e2
unknown <tobias.elinder@gmail.com>
2015-02-13 12:34:23 +0100

    lagt till grundläggande saker

---
+++ src/server/ServerMain.java
@@ -0,0 +1,12 @@
+package server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class ServerMain {
+	
+	public static void main(String args[]) throws IOException{
+		ServerSocket socket = new ServerSocket();
+	}
+
+}
