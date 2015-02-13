3858c188bbb7bbf29d057119da64424b0cf7ebb6
unknown <tobias.elinder@gmail.com>
2015-02-13 12:56:36 +0100

    lagt till lite info

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ Projektet består huvudsakligen av 3 delar:
 1. Klient som ansluter till databasen och kan skicka kommandon.
 2. Server/databasen som blir ansluten till, och hanterar kommandon och privilegier.
 3. Säkerhetscertifikat.
+
+Lite l�nkar:
+
+http://www.ibm.com/developerworks/java/tutorials/j-jsse/j-jsse.html
--- src/server/ServerMain.java
+++ src/server/ServerMain.java
@@ -1,12 +1,27 @@
 package server;
 
 import java.io.IOException;
-import java.net.ServerSocket;
+import java.nio.channels.SocketChannel;
+import java.security.*;
 
 public class ServerMain {
-	
-	public static void main(String args[]) throws IOException{
-		ServerSocket socket = new ServerSocket();
+
+	public static void main(String[] args) {
+		printOutInterestingInfo();
+
+	}
+
+	public static void printOutInterestingInfo() {
+		try {
+			Class.forName("com.sun.net.ssl.internal.ssl.Provider");
+		} catch (Exception e) {
+			System.out.println("JSSE is NOT installed correctly!");
+			System.exit(1);
+		}
+		System.out.println("JSSE is installed correctly!");
+		String trustStore = System.getProperty("javax.net.ssl.trustStore");
+		if (trustStore == null)
+			System.out.println("javax.net.ssl.trustStore is not defined");
 	}
 
 }
