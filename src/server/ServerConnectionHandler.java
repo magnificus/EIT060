54cc0ed56a456f4a2aa18f618b6b3c74013920da
unknown <tobias.elinder@gmail.com>
2015-02-13 14:31:31 +0100

    basic funktionalitet för connection

---
+++ src/server/ServerConnectionHandler.java
@@ -0,0 +1,116 @@
+package server;
+
+import java.io.*;
+import java.net.*;
+import java.security.KeyStore;
+
+import javax.net.*;
+import javax.net.ssl.*;
+import javax.security.cert.X509Certificate;
+
+public class ServerConnectionHandler implements Runnable {
+	private ServerSocket serverSocket = null;
+	private static int numConnectedClients = 0;
+
+	public ServerConnectionHandler(ServerSocket ss) throws IOException {
+		serverSocket = ss;
+		newListener();
+	}
+
+	public void run() {
+		try {
+			SSLSocket socket = (SSLSocket) serverSocket.accept();
+			newListener();
+			SSLSession session = socket.getSession();
+			X509Certificate cert = (X509Certificate) session.getPeerCertificateChain()[0];
+			String subject = cert.getSubjectDN().getName();
+			numConnectedClients++;
+			System.out.println("client connected");
+			System.out.println("client name (cert subject DN field): " + subject);
+			System.out.println(numConnectedClients + " concurrent connection(s)\n");
+
+			PrintWriter out = null;
+			BufferedReader in = null;
+			out = new PrintWriter(socket.getOutputStream(), true);
+			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+			String clientMsg = null;
+			while ((clientMsg = in.readLine()) != null) {
+				System.out.println("received '" + clientMsg + "' from client");
+				handleInput(clientMsg);
+				// System.out.print("sending '" + rev + "' to client...");
+				// out.println(rev);
+				// out.flush();
+				// System.out.println("done\n");
+			}
+			in.close();
+			out.close();
+			socket.close();
+			numConnectedClients--;
+			System.out.println("client disconnected");
+			System.out.println(numConnectedClients + " concurrent connection(s)\n");
+		} catch (IOException e) {
+			System.out.println("Client died: " + e.getMessage());
+			e.printStackTrace();
+			return;
+		}
+	}
+
+	private void newListener() {
+		(new Thread(this)).start();
+	} // calls run()
+
+	public static void main(String args[]) {
+		System.out.println("\nServer Started\n");
+		int port = 8888;
+		String type = "TLS";
+		try {
+			ServerSocketFactory ssf = getServerSocketFactory(type);
+			ServerSocket ss = ssf.createServerSocket(port);
+			((SSLServerSocket) ss).setNeedClientAuth(true); // enables client
+															// authentication
+			new ServerConnectionHandler(ss);
+		} catch (IOException e) {
+			System.out.println("Unable to start Server: " + e.getMessage());
+			e.printStackTrace();
+		}
+	}
+
+	private static ServerSocketFactory getServerSocketFactory(String type) {
+		System.out.println("Working Directory = " + System.getProperty("user.dir"));
+
+		if (type.equals("TLS")) {
+			SSLServerSocketFactory ssf = null;
+			try { // set up key manager to perform server authentication
+				SSLContext ctx = SSLContext.getInstance("TLS");
+				KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
+				TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
+				KeyStore ks = KeyStore.getInstance("JKS");
+				KeyStore ts = KeyStore.getInstance("JKS");
+				char[] password = "password".toCharArray();
+
+				ks.load(new FileInputStream("lib_server/serverkeystore"), password); // keystore
+				// password
+				// (storepass)
+				ts.load(new FileInputStream("lib_server/servertruststore"), password); // truststore
+																						// password
+																						// (storepass)
+				kmf.init(ks, password); // certificate password (keypass)
+				tmf.init(ts); // possible to use keystore as truststore here
+				ctx.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
+				ssf = ctx.getServerSocketFactory();
+				return ssf;
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else {
+			return ServerSocketFactory.getDefault();
+		}
+		return null;
+	}
+
+	private void handleInput(String clientMsg) {
+		// TODO Auto-generated method stub
+
+	}
+}
